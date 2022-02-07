4cc2ead7249c7fc97d6d8f78c6991c86116b9d17
Rozer-Mutum <Rozer.Mutum@yokogawa.com>
2022-02-07 19:01:42 +0530

    New change

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace superfancy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
